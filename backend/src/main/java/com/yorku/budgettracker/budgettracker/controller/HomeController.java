cc2e7b298558e0ce9ec71b7beca0e2273ef04d9d
Iyinoluwa Olu-Alabi <99039925+iyinno@users.noreply.github.com>
2026-02-11 17:40:36 -0500

    Create HomeController.java
    Add HomeController to serve GUI page

---
+++ backend/src/main/java/com/yorku/budgettracker/budgettracker/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.yorku.budgettracker.budgettracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
